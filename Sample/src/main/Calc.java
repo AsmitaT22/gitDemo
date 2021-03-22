4dddb5fb36b32f7555c6d1b8cc11df281b732324
Asmita <Asmita@LAPTOP-9KVN43KH>
2021-03-22 18:26:41 +0530

    commit3

--- Sample/src/main/Calc.java
+++ Sample/src/main/Calc.java
@@ -6,6 +6,9 @@ public class Calc {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello");
+		add(5,4);
 	}
-
+	public static void add(int num1,int num2){
+	      System.out.println(num1+num2);
+	  }
 }
